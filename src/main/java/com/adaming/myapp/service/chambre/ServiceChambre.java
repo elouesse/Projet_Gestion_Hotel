eece35a03a0d89e9d56d5e7f38e30fe41b7f1aac
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 17:54:48 +0200

    Service chambre

--- src/main/java/com/adaming/myapp/service/chambre/ServiceChambre.java
+++ src/main/java/com/adaming/myapp/service/chambre/ServiceChambre.java
@@ -1,21 +1,68 @@
-package com.adaming.myapp.service.chambre;
+package com.adaming.myapp.dao.chambre;
 
+import java.util.List;
 import java.util.logging.Logger;
 
-import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
-import com.adaming.myapp.dao.chambre.IdaoChambre;
+import com.adaming.myapp.entities.Chambre;
+import com.adaming.myapp.entities.Hotel;
 
-@Transactional
-public class ServiceChambre implements IserviceChambre {
-	
-	private IdaoChambre dao;
-	Logger log = Logger.getLogger("ServiceChambre");
-	
-	public void setDao(IdaoChambre dao) {
-		this.dao = dao;
-		log.info("<--------------daoChambre injected------------------>");
+public class DaoChambre implements IdaoChambre {
+
+	Logger log = Logger.getLogger("DaoChambre");
+	@PersistenceContext
+	private EntityManager em;
+
+	@Override
+	public Chambre addChambre(Chambre ch, Long idHotel) {
+		Hotel h = em.find(Hotel.class, idHotel);
+		ch.setHotel(h);
+		em.persist(ch);
+		log.info("La chambre "+ch.getNumeroChambre() + " a bien ete enregistre dans l'hotel "+h.getNomHotel());
+		return ch;
 	}
-	
+
+	@Override
+	public Chambre deleteChambre(Long idChambre) {
+		Chambre ch = em.find(Chambre.class, idChambre);
+		em.remove(ch);
+		log.info("La chambre "+ch.getNumeroChambre() +" a bien ete supprime.");
+		return ch;
+	}
+
+	@Override
+	public Chambre updateChambre(Chambre ch) {
+		em.merge(ch);
+		log.info("La chambre "+ch.getNumeroChambre()+ " a bien ete modifie.");
+		return ch;
+	}
+
+	@Override
+	public Chambre getChambre(Long idChambre) {
+		Chambre ch = em.find(Chambre.class, idChambre);
+		log.info("La chambre "+ch.getNumeroChambre()+ " a bien ete recupere.");
+		return ch;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Chambre> getChambres() {
+		Query req = em.createQuery("from Chambre");
+		log.info("Il existe une liste de " + req.getResultList().size()
+				+ " de Chambres");
+		return req.getResultList();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Chambre> getDispoChambres() {
+		Query req = em.createQuery("from Chambre where reserv is null");
+		log.info("Il existe une liste de " + req.getResultList().size()
+				+ " de Chambres disponibles");
+		return req.getResultList();
+	}
 
 }
