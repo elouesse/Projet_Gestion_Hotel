d5b714c7bc5ba0830600d0b9271cf0d9600dde13
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 17:52:09 +0200

    Commit reservation tz

--- src/main/java/com/adaming/myapp/dao/reservation/DaoReservation.java
+++ src/main/java/com/adaming/myapp/dao/reservation/DaoReservation.java
@@ -1,15 +1,78 @@
 package com.adaming.myapp.dao.reservation;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
+import com.adaming.myapp.entities.*;
 
 public class DaoReservation implements IdaoReservation {
-	
+
 	Logger log = Logger.getLogger("DaoReservation");
 	@PersistenceContext
 	private EntityManager em;
+
+	@Override
+	public Reservation addReservation(Reservation r, Long idC, Long idCh,
+			Long idP) {
+		Personne c = em.find(Personne.class, idC);
+		r.setPersonne(c);
+		Chambre ch = em.find(Chambre.class, idCh);
+		r.setChambre(ch);
+		Produit p = em.find(Produit.class, idP);
+		Consommation co = new Consommation(1);
+		co.setProduit(p);
+		r.getConsom().add(co);
+		em.persist(r);
+		log.info("La reservation " + r.getIdReservation()
+				+ " a bien ete enregistre .");
+		return r;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Reservation> getReservations(Long idC) {
+		Query req = em.createQuery("from Reservation where idPersonne=:idC");
+		req.setParameter("idC", idC);
+		log.info("Il existe une liste de " + req.getResultList().size()
+				+ " de Reservations");
+		return req.getResultList();
+	}
+
+	@Override
+	public Reservation deleteReservation(Long idR) {
+		Reservation r = em.find(Reservation.class, idR);
+		em.remove(r);
+		log.info("La reservation "+r.getIdReservation() +" a bien ete supprime.");
+		return r;
+	}
+
+	@Override
+	public Reservation updateReservation(Reservation r) {
+		em.merge(r);
+		log.info("La reservation "+r.getIdReservation()+ " a bien ete modifie.");
+		return r;
+	}
+
+	@Override
+	public Double getTotalCostReservation(Long idR) {
+		Double 
+		return null;
+	}
+
+	@Override
+	public Double getTotalCostReservations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Consommation> getStockProduit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/main/java/com/adaming/myapp/dao/reservation/IdaoReservation.java
+++ src/main/java/com/adaming/myapp/dao/reservation/IdaoReservation.java
@@ -1,5 +1,16 @@
 package com.adaming.myapp.dao.reservation;
 
-public interface IdaoReservation {
+import java.util.List;
 
+import com.adaming.myapp.entities.*;
+
+public interface IdaoReservation {
+	
+	public Reservation addReservation(Reservation r, Long idC, Long idCh, Long idP);
+	public List<Reservation> getReservations(Long idC);
+	public Reservation deleteReservation(Long idR);
+	public Reservation updateReservation(Reservation r);
+	public Double getTotalCostReservation(Long idR);
+	public Double getTotalCostReservations();
+	public List<Consommation> getStockProduit();
 }
