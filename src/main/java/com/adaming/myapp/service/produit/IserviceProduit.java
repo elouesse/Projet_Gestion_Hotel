e25c16f5a4efa546a21e09b875e66cc016ea1fa8
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 18:07:53 +0200

    Service Produit tz

--- src/main/java/com/adaming/myapp/service/produit/IserviceProduit.java
+++ src/main/java/com/adaming/myapp/service/produit/IserviceProduit.java
@@ -1,5 +1,21 @@
 package com.adaming.myapp.service.produit;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Produit;
+/**
+ * Nom Classe: Idaoproduit
+ * @author Eli, Thierry
+ * Version: 1.0
+ * Date: 11/07/2016
+ *
+ */
 public interface IserviceProduit {
+	
+	public Produit addProduit(Produit p);
+	public Produit updateProduit(Produit p);
+	public Produit deleteProduit(Long idProduit);
+	public List<Produit> getListeDesProduits();
+	public Produit getProduitParId(Long idProduit);
 
 }
