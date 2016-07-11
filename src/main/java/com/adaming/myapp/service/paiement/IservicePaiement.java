7a97692f6e2718e0b283b583f60016e101b509af
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 18:06:46 +0200

    Service Paiement tz

--- src/main/java/com/adaming/myapp/service/paiement/IservicePaiement.java
+++ src/main/java/com/adaming/myapp/service/paiement/IservicePaiement.java
@@ -1,5 +1,17 @@
 package com.adaming.myapp.service.paiement;
 
+import com.adaming.myapp.entities.Paiement;
+
+/**
+ * Nom Classe: Idaoproduit
+ * @author Eli, Thierry
+ * Version: 1.0
+ * Date: 11/07/2016
+ *
+ */
 public interface IservicePaiement {
+	
+	public Paiement addPaiement(Paiement p, Long idFacture);
+	public Double getBeneficeAnnuelle(int annee);
 
 }
