ff3b8185aa3f7976b244d6bb743df16d24e3739e
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 17:47:57 +0200

    commit dao tz

--- src/main/java/com/adaming/myapp/dao/facture/IdaoFacture.java
+++ src/main/java/com/adaming/myapp/dao/facture/IdaoFacture.java
@@ -1,5 +1,20 @@
 package com.adaming.myapp.dao.facture;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Facture;
+/**
+ * Nom Classe: Reservation
+ * @author Eli, Thierry
+ * Version: 1.0
+ * Date: 11/07/2016
+ *
+ */
 public interface IdaoFacture {
+	
+	public Facture addFacture(Facture f,Long idReservation, Long idHotel);
+	public List<Facture> getListeDesFacturesParClient(Long idPersonne);
+	public Double coutAnnuelParClient(Long idPersonne, int annee);
+	public Double coutSemestrielParClient(Long idPersonne, int annee,int semestre);
 
 }
