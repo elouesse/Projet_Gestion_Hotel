da44e9500597089575f1ecd6592ba2ba3d8f7b30
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 18:10:04 +0200

    Personne tz

--- src/main/java/com/adaming/myapp/dao/person/IdaoPersonne.java
+++ src/main/java/com/adaming/myapp/dao/person/IdaoPersonne.java
@@ -1,5 +1,34 @@
 package com.adaming.myapp.dao.person;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Personne;
+
+/**
+ * Nom Classe: Idaoproduit
+ * @author Eli, Thierry
+ * Version: 1.0
+ * Date: 11/07/2016
+ *
+ */
+
+
 public interface IdaoPersonne {
+	
+	public Personne addPersonne(Personne p, Long idHotel);
+	public Personne updatePersonne(Personne p);
+	public Personne deletePersonne(Long id);
+	public Personne getPersonne(Long idPersonne);
+	public List<Personne> getPersonnes();
+	public List<Personne> getPersonneByMc(String mc);
+	
+	/**
+	 * addPersonne
+	*supprimer une Personne
+	*mettre a jour une Personne
+	*récupérer la liste de toutes les Personne
+	*récupérer une Personne par Id
+	*récupérer la liste des Personne par mot-cle
+	**/
 
 }
