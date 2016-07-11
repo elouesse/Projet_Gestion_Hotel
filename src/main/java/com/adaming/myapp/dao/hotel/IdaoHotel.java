205cb243990ad4dbcfbf65f7a203c63eaec4139a
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-11 17:49:49 +0200

    commit Dao Hotel TZ

--- src/main/java/com/adaming/myapp/dao/hotel/IdaoHotel.java
+++ src/main/java/com/adaming/myapp/dao/hotel/IdaoHotel.java
@@ -1,5 +1,22 @@
 package com.adaming.myapp.dao.hotel;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Client;
+import com.adaming.myapp.entities.Employe;
+import com.adaming.myapp.entities.Hotel;
+
+/**
+ * Nom Classe: Idaoproduit
+ * @author Eli, Thierry
+ * Version: 1.0
+ * Date: 11/07/2016
+ *
+ */
 public interface IdaoHotel {
+	
+	public Hotel addHotel(Hotel h);
+	public List<Employe> getListeDesEmployes(Long idHotel);
+	public List<Client> getListeDesClients(Long idHotel);
 
 }
