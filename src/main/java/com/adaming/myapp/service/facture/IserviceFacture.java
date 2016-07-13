5b79febb73457750f80384e16942335f294641ac
inti0299 <inti0299@DESKTOP-NIFRS8F>
2016-07-13 13:03:58 +0200

    Commit Facture dao + I

--- src/main/java/com/adaming/myapp/service/facture/IserviceFacture.java
+++ src/main/java/com/adaming/myapp/service/facture/IserviceFacture.java
@@ -1,5 +1,13 @@
 package com.adaming.myapp.service.facture;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Facture;
+
 public interface IserviceFacture {
+	public Facture addFacture(Facture f,Long idReservation, Long idHotel);
+	public List<Facture> getListeDesFacturesParClient(Long idPersonne);
+	public Double coutAnnuelParClient(Long idPersonne, int annee);
+	public Double coutSemestrielParClient(Long idPersonne, int annee,int semestre);
 
 }
--- src/main/java/com/adaming/myapp/service/facture/ServiceFacture.java
+++ src/main/java/com/adaming/myapp/service/facture/ServiceFacture.java
@@ -1,10 +1,12 @@
 package com.adaming.myapp.service.facture;
 
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.transaction.annotation.Transactional;
 
 import com.adaming.myapp.dao.facture.IdaoFacture;
+import com.adaming.myapp.entities.Facture;
 
 
 @Transactional
@@ -16,6 +18,28 @@ public class ServiceFacture implements IserviceFacture {
 	public void setDao(IdaoFacture dao) {
 		this.dao = dao;
 		log.info("<--------------daoFacture injected------------------>");
+	}
+
+	@Override
+	public Facture addFacture(Facture f, Long idReservation, Long idHotel) {
+		
+		return dao.addFacture(f, idReservation, idHotel);
+	}
+
+	@Override
+	public List<Facture> getListeDesFacturesParClient(Long idPersonne) {
+		return dao.getListeDesFacturesParClient(idPersonne);
+	}
+
+	@Override
+	public Double coutAnnuelParClient(Long idPersonne, int annee) {
+		return dao.coutAnnuelParClient(idPersonne, annee);
+	}
+
+	@Override
+	public Double coutSemestrielParClient(Long idPersonne, int annee,
+			int semestre) {
+		return dao.coutSemestrielParClient(idPersonne, annee, semestre);
 	}
 
 }
